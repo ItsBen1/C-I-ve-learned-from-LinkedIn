778d8fcc24a238d9970939213bba35184705654b
Virgilian Lacy <67180647+ItsBen1@users.noreply.github.com>
2020-10-30 22:45:31 -0700

    Add files via upload

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            for (int x = 0; x < 5; x++)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                  Console.WriteLine(i);
+                }
+                for (int i = 10; i >= 1; i--)
+                {
+                  Console.WriteLine(i);
+                }
+            }
+            }
+        }
+    }
